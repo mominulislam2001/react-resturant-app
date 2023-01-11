1a7334970b99806fba2230064aad4d8919c35a33
Ayan <islamayan123456@gmail.com>
2023-01-12 01:09:26 +0600

    first commit

---
+++ src/components/MainComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from './footer/Footer';
+import Header from './header/Header';
+import Body from './body/Body';
+const MainComponent = () => {
+  return (
+    <div>
+      <Header />  
+      <Body />
+      <Footer />
+    </div>
+  )
+}
+
+export default MainComponent;
